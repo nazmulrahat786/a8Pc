bd40b88145d7e767480164929d905429f2a3b27b
Nazmul Haque Rahat <nazmulrahat786.13@gmail.com>
2026-05-18 23:21:16 +0600

    Update MainPage.jsx

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import { Outlet } from 'react-router';
-import Footer from '../Components/Footer';
+import Footer from '../Components/Footer'; 
  
 const MainPage = () => { 
     return ( 
