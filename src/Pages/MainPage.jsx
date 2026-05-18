6ab283869596c8d4f896e78f5b45f6e56df723b7
Nazmul Haque Rahat <nazmulrahat786.13@gmail.com>
2026-05-18 23:20:53 +0600

    Update MainPage.jsx

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router';
 import Footer from '../Components/Footer';
  
 const MainPage = () => {
-    return (
+    return ( 
         <div className='max-w-[1280px] mx-auto flex bg-gray-50 flex-col justify-between min-h-screen'>
            <div>
              <Navbar></Navbar>
