88db52299ab111e5e808b7630c5f413205254f55
Nazmul Haque Rahat <nazmulrahat786.13@gmail.com>
2026-05-18 23:21:30 +0600

    Format MainPage component and clean up imports

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from '../Components/Navbar';
 import { Outlet } from 'react-router';
 import Footer from '../Components/Footer'; 
- 
+  
 const MainPage = () => { 
     return ( 
         <div className='max-w-[1280px] mx-auto flex bg-gray-50 flex-col justify-between min-h-screen'>
